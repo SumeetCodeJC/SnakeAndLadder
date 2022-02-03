0200b84402cd82bbcaf29ed48daa690e636e8535
SumeetCodeJC <code.sumeet9@gmail.com>
2022-02-03 17:52:43 +0530

    Modified : Die Roll

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,14 +1,22 @@
 package com.bridgelabz.snakeandladder;
 
 public class SnakeAndLadder {
-	//Declaring Variables for Players Position
+
+    //Declaring Variables for Players Position
     int playerPosition = 0;
 
     //Method to Show Player Current Position
     private void showPosition() {
         //Displaying Player Position
-        System.out.println("Player Starting Position = "+ playerPosition);
+        System.out.println("Player Starting Position : "+ playerPosition);
     }
+
+    //Method to Roll the Die and Get No between 1 to 6
+    private int rollDie() {
+        int dieNo = (int) Math.floor(Math.random() * 10)%6+1;
+        return dieNo;
+    }
+
     public static void main(String[] args) {
         //Displaying Welcome Message
         System.out.println("Welcome to Snake and Ladder Simulator Problem.");
@@ -18,7 +26,8 @@ public class SnakeAndLadder {
 
         //Showing Player Position
         player1.showPosition();
+
+        //Show Die Rolling No
+        System.out.println("Die Number for Player : "+ player1.rollDie());
     }
 }
-
-
